3e01c9c75efeded0cfd3c429771a8bfa6876a6c8
zhuangjiahua <827126983@qq.com>
2025-06-09 17:20:10 +0800

    Update NavigationBarObserver.java

--- immersionbar/src/main/java/com/gyf/immersionbar/NavigationBarObserver.java
+++ immersionbar/src/main/java/com/gyf/immersionbar/NavigationBarObserver.java
@@ -1,15 +1,6 @@
 package com.gyf.immersionbar;
 
 import static com.gyf.immersionbar.Constants.IMMERSION_NAVIGATION_BAR_MODE_DEFAULT;
-import static com.gyf.immersionbar.Constants.IMMERSION_NAVIGATION_BAR_MODE_EMUI;
-import static com.gyf.immersionbar.Constants.IMMERSION_NAVIGATION_BAR_MODE_MIUI;
-import static com.gyf.immersionbar.Constants.IMMERSION_NAVIGATION_BAR_MODE_MIUI_HIDE;
-import static com.gyf.immersionbar.Constants.IMMERSION_NAVIGATION_BAR_MODE_OPPO;
-import static com.gyf.immersionbar.Constants.IMMERSION_NAVIGATION_BAR_MODE_SAMSUNG;
-import static com.gyf.immersionbar.Constants.IMMERSION_NAVIGATION_BAR_MODE_SAMSUNG_GESTURE;
-import static com.gyf.immersionbar.Constants.IMMERSION_NAVIGATION_BAR_MODE_SAMSUNG_GESTURE_TYPE;
-import static com.gyf.immersionbar.Constants.IMMERSION_NAVIGATION_BAR_MODE_SAMSUNG_OLD;
-import static com.gyf.immersionbar.Constants.IMMERSION_NAVIGATION_BAR_MODE_VIVO;
 
 import android.app.Application;
 import android.database.ContentObserver;
@@ -48,31 +39,7 @@ final class NavigationBarObserver extends ContentObserver {
             Uri uri;
             Uri uri1 = null;
             Uri uri2 = null;
-            if (OSUtils.isHuaWei() || OSUtils.isEMUI()) {
-                if (OSUtils.isEMUI3_x() || Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-                    uri = Settings.System.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_EMUI);
-                } else {
-                    uri = Settings.Global.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_EMUI);
-                }
-            } else if (OSUtils.isXiaoMi() || OSUtils.isMIUI()) {
-                uri = Settings.Global.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_MIUI);
-                uri1 = Settings.Global.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_MIUI_HIDE);
-            } else if (OSUtils.isVivo() || OSUtils.isFuntouchOrOriginOs()) {
-                uri = Settings.Secure.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_VIVO);
-            } else if (OSUtils.isOppo() || OSUtils.isColorOs()) {
-                uri = Settings.Secure.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_OPPO);
-            } else if (OSUtils.isSamsung()) {
-                int i = Settings.Global.getInt(mApplication.getContentResolver(), IMMERSION_NAVIGATION_BAR_MODE_SAMSUNG_OLD, -1);
-                if (i == -1) {
-                    uri = Settings.Global.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_SAMSUNG);
-                    uri1 = Settings.Global.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_SAMSUNG_GESTURE_TYPE);
-                    uri2 = Settings.Global.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_SAMSUNG_GESTURE);
-                } else {
-                    uri = Settings.Global.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_SAMSUNG_OLD);
-                }
-            } else {
-                uri = Settings.Secure.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_DEFAULT);
-            }
+            uri = Settings.Secure.getUriFor(IMMERSION_NAVIGATION_BAR_MODE_DEFAULT);
             if (uri != null) {
                 mApplication.getContentResolver().registerContentObserver(uri, true, this);
                 mIsRegister = true;
